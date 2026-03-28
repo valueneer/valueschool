2d7a8a9fe3ced3958f68494f3ceb20a6531eff54
Hendrik Martens <hendrik@valueverse.de>
2026-03-28 19:17:43 +0100

    Update Value School

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d C:\Users\hendr\claude\landingpage
+git add .
+git commit -m "Update Value School"
+git push
+echo.
+echo === Fertig! Seite wird in 30 Sekunden live sein. ===
+echo.
+pause
